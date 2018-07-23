7ebafafdfb1b8bb572bad20bc3ace3d28943c76a
harryyy27 <harryyy27@gmail.com>
2018-07-23 14:50:01 +0100

    handled html

--- src/server.js
+++ src/server.js
@@ -1,11 +1,21 @@
 var http = require("http");
+var fs = require("fs");
 
 var message = "Harry and Jessie are lovin' the Node Girls workshop!";
 
 function handler(request, response) {
-  response.writeHead(200, { "Content-Type": "text/html" });
-  response.write(message);
-  response.end();
+  var endpoint = request.url;
+  if (endpoint === "/") {
+    response.writeHead(200, { "Content-Type": "text/html" });
+
+    fs.readFile(__dirname + "/../public/index.html", function(error, file) {
+      if (error) {
+        console.log(error);
+        return;
+      }
+      response.end(file);
+    });
+  }
 }
 var server = http.createServer(handler);
 
