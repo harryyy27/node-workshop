5d09b7de7d8e40ede5f642241c07519e551457af
harryyy27 <harryyy27@gmail.com>
2018-07-23 15:40:03 +0100

    other route handlers added

--- src/server.js
+++ src/server.js
@@ -4,11 +4,31 @@ var fs = require("fs");
 var message = "Harry and Jessie are lovin' the Node Girls workshop!";
 
 function handler(request, response) {
+  var extension = {
+    html: "text/html",
+    css: "text/css",
+    jpg: "image/jpg",
+    png: "image/png",
+    js: "text/javascript",
+    ico: "image/x-icon"
+  };
   var endpoint = request.url;
+
   if (endpoint === "/") {
     response.writeHead(200, { "Content-Type": "text/html" });
 
     fs.readFile(__dirname + "/../public/index.html", function(error, file) {
+      if (error) {
+        console.log(error);
+        return;
+      }
+      response.end(file);
+    });
+  } else {
+    var key = endpoint.split(".")[1];
+    response.writeHead(200, `Content-Type: ${extension[key]}`);
+
+    fs.readFile(__dirname + `/../public${endpoint}`, function(error, file) {
       if (error) {
         console.log(error);
         return;
